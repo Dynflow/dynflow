c2afab2766a95bc838f85ab69e8fbc2931b5abcb
Petr Chalupa <git@pitr.ch>
2013-07-26 11:34:31 +0200

    remove RunPlan dependency on persistence, fix test_helper not to use RunStep#action

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,27 +9,27 @@ require 'pry'
 
 module PlanAssertions
 
-  def inspect_flow(out, flow, prefix)
+  def inspect_flow(out, execution_plan, flow, prefix)
+    flow
     case flow
     when Dynflow::Flows::Atom
       out << prefix
       out << flow.step.id.to_s << ': '
-      action = flow.step.action
-      out << action.class.superclass.name[/\w+\Z/]
+      out << flow.step.action_class.action_class.to_s[/\w+\Z/]
       out << ' '
-      out << action.input.inspect
+      out << execution_plan.world.persistence_adapter.load_action(execution_plan.id, flow.step.action_id)[:input].inspect
       out << "\n"
     else
       out << prefix << flow.class.name << "\n"
       flow.sub_flows.each do |sub_flow|
-        inspect_flow(out, sub_flow, prefix + "  ")
+        inspect_flow(out, execution_plan, sub_flow, prefix + "  ")
       end
     end
   end
 
   def assert_run_plan(expected, execution_plan)
     plan_string = ""
-    inspect_flow(plan_string, execution_plan.run_flow, "")
+    inspect_flow(plan_string, execution_plan, execution_plan.run_flow, "")
     plan_string.chomp.must_equal expected.chomp
   end
 
