88421752dd2fd058df7298379819eef0bd34ab69
Ivan Necas <necasik@gmail.com>
2013-07-29 15:46:57 +0200

    Simple test for web console
    Just check that the results are ok (no output validation)

---
+++ test/web_console_test.rb
@@ -0,0 +1,40 @@
+require_relative 'test_helper'
+require_relative 'code_workflow_example'
+require 'dynflow/web_console'
+
+require 'rack/test'
+ENV['RACK_ENV'] = 'test'
+
+module Dynflow
+  describe 'web console' do
+
+    include Rack::Test::Methods
+
+    let :world do
+      SimpleWorld.new
+    end
+
+    let :execution_plan_id do
+      id, plan = world.trigger(CodeWorkflowExample::FastCommit, 'sha' => 'abc123')
+      plan.wait
+      id
+    end
+
+    let :app do
+      world = self.world
+      Dynflow::WebConsole.setup do
+        set :world, world
+      end
+    end
+
+    it 'lists all execution plans' do
+      get '/'
+      assert last_response.ok?
+    end
+
+    it 'show an execution plan' do
+      get "/#{execution_plan_id}"
+      assert last_response.ok?
+    end
+  end
+end
