69288d415fb2a7cc6da54ff3baa937bc8e4efab4
Petr Chalupa <git@pitr.ch>
2014-01-09 11:12:58 +0100

    Do not send Accept after Done or twice

--- lib/dynflow/listeners/socket.rb
+++ lib/dynflow/listeners/socket.rb
@@ -23,32 +23,42 @@ module Dynflow
         ios                   = [@server, *@clients]
         reads, writes, errors = IO.select(ios, [], ios)
         reads.each do |readable|
-
           if readable == @server
             add_client @server.accept
             logger.info 'Client connected.'
 
           else
             match message = receive_message(readable),
-                  Execute.(~any, ~any) >-> id, uuid do
-                    begin
-                      @world.execute(uuid,
-                                     Future.new do |_|
-                                       send_message_to_client readable, Done[id, uuid]
-                                     end)
-                      send_message_to_client readable, Accepted[id]
-                    rescue Dynflow::Error => e
-                      send_message_to_client readable, Failed[id, e.message]
-                    end
-                  end,
-                  NilClass.to_m >-> do
+                  (on ~Execute do |(id, uuid)|
+                    execute(readable, id, uuid)
+                  end),
+                  (on NilClass.to_m do
                     remove_client readable
                     logger.info 'Client disconnected.'
-                  end
+                  end)
           end
         end
       rescue => error
         logger.fatal error
+      end
+
+      def execute(readable, id, uuid)
+        accepted = false
+        accept   = -> do
+          unless accepted
+            accepted = true
+            send_message_to_client readable, Accepted[id]
+          end
+        end
+
+        @world.execute(uuid,
+                       Future.new do |_|
+                         accept.call
+                         send_message_to_client readable, Done[id, uuid]
+                       end)
+        accept.call
+      rescue Dynflow::Error => e
+        send_message_to_client readable, Failed[id, e.message]
       end
 
       def add_client(client)
