144b1b8acc1ecf680ba84d2930b4bfc303a3ebe9
Petr Chalupa <git@pitr.ch>
2014-02-17 19:31:58 +0100

    whitespace

--- web/assets/javascripts/application.js
+++ web/assets/javascripts/application.js
@@ -1,25 +1,26 @@
-(function($) {
-    $.fn.extend({
-        postlink: function(options) {
-            return this.each(function() {
-                $(this).click(function(e) {
-                    var frm = $("<form>");
-                    frm.attr({'action':$(this).attr('href'), 'method': 'post'});
-                    frm.appendTo("body");
-                    frm.submit();
-                    e.preventDefault();
-                });
-            });
-        }
+(function ($) {
+  $.fn.extend({
+    postlink: function (options) {
+      return this.each(function () {
+        $(this).click(function (e) {
+          var frm = $("<form>");
+          frm.attr({'action': $(this).attr('href'), 'method': 'post'});
+          frm.appendTo("body");
+          frm.submit();
+          e.preventDefault();
+        });
+      });
+    }
+  });
+
+  $(function () {
+    $('.postlink').postlink();
+
+    $('table.flow span.step-label').click(function (e) {
+      var stepData = $(this).siblings('div.action');
+      stepData.slideToggle();
     });
 
-    $(function() {
-        $('.postlink').postlink();
-
-        $('table.flow span.step-label').click(function (e) {
-            var stepData = $(this).siblings('div.action');
-            stepData.slideToggle();
-        });
     $('.plan-step .step-label').click(function (e) {
       var stepData = $(this).siblings('div.action');
       stepData.slideToggle();
