c6c33e02eaa8d733b850929a3b2972d90f66dab8
Ivan Nečas <necasik@gmail.com>
2014-06-05 14:38:32 +0200

    handle failing to constantize exceptions class

--- lib/dynflow/errors.rb
+++ lib/dynflow/errors.rb
@@ -1,5 +1,28 @@
 module Dynflow
   module Errors
     class RescueError < StandardError; end
+
+    # placeholder in case the deserialized error is no longer available
+    class UnknownError < StandardError
+      def self.for_exception_class(class_name)
+        Class.new(self) do
+          define_singleton_method :name do
+            class_name
+          end
+        end
+      end
+
+      def self.inspect
+        "#{UnknownError.name}[#{name}]"
+      end
+
+      def self.to_s
+        inspect
+      end
+
+      def inspect
+        "#{self.class.inspect}: #{message}"
+      end
+    end
   end
 end
