4521c218a0fa9bd3f5c71d10550af6d16ab8083e
Ivan Nečas <necasik@gmail.com>
2015-02-02 16:05:59 +0100

    Execution history tab

--- examples/orchestrate_evented.rb
+++ examples/orchestrate_evented.rb
@@ -153,7 +153,8 @@ module OrchestrateEvented
         puts <<-MSG.gsub(/^.*\|/, '')
 
             | Execution plan #{execution_plan_id} got stuck
-            | You can cancel the stucked step at http://localhost:4567/#{execution_plan_id}
+            | You can cancel the stucked step at
+            | http://localhost:4567/#{execution_plan_id}
 
         MSG
         # we suspend the action but don't plan the wakeup event,
