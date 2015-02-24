4688fde3ad716184dac49ca075b6fc0a7642912f
Ivan Nečas <necasik@gmail.com>
2015-02-24 10:10:35 +0100

    Add tests for loading/saving steps in persistence

--- test/persistence_test.rb
+++ test/persistence_test.rb
@@ -4,15 +4,49 @@ require 'fileutils'
 module Dynflow
   module PersistenceTest
     describe 'persistence adapters' do
+
+      let :execution_plans_data do
+        [{ id: 'plan1', state: 'paused' },
+         { id: 'plan2', state: 'stopped' },
+         { id: 'plan3', state: 'paused' }]
+      end
+
+      let :action_data do
+        { id: 1, caller_execution_plan_id: nil, caller_action_id: nil }
+      end
+
+      let :step_data do
+        { id: 1,
+          state: 'success',
+          started_at: '2015-02-24 10:00',
+          ended_at: '2015-02-24 10:01',
+          real_time: 1.1,
+          execution_time: 0.1,
+          action_id: 1,
+          progress_done: 1,
+          progress_weight: 2.5 }
+      end
+
       def prepare_plans
-        proto_plans = [{ id: 'plan1', state: 'paused' },
-                       { id: 'plan2', state: 'stopped' },
-                       { id: 'plan3', state: 'paused' }]
-        proto_plans.map do |h|
+        execution_plans_data.map do |h|
           h.merge result:    nil, started_at: (Time.now-20).to_s, ended_at: (Time.now-10).to_s,
               real_time: 0.0, execution_time: 0.0
         end.tap do |plans|
           plans.each { |plan| adapter.save_execution_plan(plan[:id], plan) }
+        end
+      end
+
+      def prepare_action(plan)
+        adapter.save_action(plan, action_data[:id], action_data)
+      end
+
+      def prepare_step(plan)
+        adapter.save_step(plan, step_data[:id], step_data)
+      end
+
+      def prepare_plans_with_actions
+        prepare_plans.map do |plan|
+          prepare_action(plan[:id])
         end
       end
 
@@ -62,12 +96,9 @@ module Dynflow
         end
 
         describe '#load_execution_plan and #save_execution_plan' do
-          it 'serializes/deserializes the plan data from the storage' do
-            plan = { id:        'plan1', state: :pending, result: nil, started_at: nil, ended_at: nil,
-                     real_time: 0.0, execution_time: 0.0 }
+          it 'serializes/deserializes the plan data' do
             -> { adapter.load_execution_plan('plan1') }.must_raise KeyError
-
-            adapter.save_execution_plan('plan1', plan)
+            prepare_plans
             adapter.load_execution_plan('plan1')[:id].must_equal 'plan1'
             adapter.load_execution_plan('plan1')['id'].must_equal 'plan1'
             adapter.load_execution_plan('plan1').keys.size.must_equal 7
@@ -78,23 +109,31 @@ module Dynflow
         end
 
         describe '#load_action and #save_action' do
-          it 'serializes/deserializes the action data from the storage' do
-            plan = { id:        'plan1', state: :pending, result: nil, started_at: nil, ended_at: nil,
-                     real_time: 0.0, execution_time: 0.0 }
-            adapter.save_execution_plan('plan1', plan)
+          it 'serializes/deserializes the action data' do
+            prepare_plans
+            action_id = action_data[:id]
+            -> { adapter.load_action('plan1', action_id) }.must_raise KeyError
 
-            action = { id: 1, caller_execution_plan_id: nil, caller_action_id: nil }
-            -> { adapter.load_action('plan1', 1) }.must_raise KeyError
+            prepare_action('plan1')
+            loaded_action = adapter.load_action('plan1', action_id)
+            loaded_action[:id].must_equal action_id
+            loaded_action.must_equal(action_data.stringify_keys)
 
-            adapter.save_action('plan1', 1, action)
-            adapter.load_action('plan1', 1)[:id].must_equal 1
-            adapter.load_action('plan1', 1)['id'].must_equal 1
-            adapter.load_action('plan1', 1).keys.must_equal %w[id caller_execution_plan_id caller_action_id]
-
-            adapter.save_action('plan1', 1, nil)
-            -> { adapter.load_action('plan1', 1) }.must_raise KeyError
+            adapter.save_action('plan1', action_id, nil)
+            -> { adapter.load_action('plan1', action_id) }.must_raise KeyError
 
             adapter.save_execution_plan('plan1', nil)
+          end
+        end
+
+        describe '#load_step and #save_step' do
+          it 'serializes/deserializes the step data' do
+            prepare_plans_with_actions
+            step_id = step_data[:id]
+            prepare_step('plan1')
+            loaded_step = adapter.load_step('plan1', step_id)
+            loaded_step[:id].must_equal step_id
+            loaded_step.must_equal(step_data.stringify_keys)
           end
         end
       end
