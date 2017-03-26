549c879e18567b280f9ef2d6d85edd5b2a790fba
Shimon Shtein <sshtein@redhat.com>
2017-03-26 13:29:40 +0300

    Added callbacks to memory watcher

--- lib/dynflow/watchers/memory_consumption_watcher.rb
+++ lib/dynflow/watchers/memory_consumption_watcher.rb
@@ -11,14 +11,19 @@ module Dynflow
         @world = world
         @polling_interval = options[:polling_interval] || 60
         @memory_info_provider = options[:memory_info_provider] || GetProcessMem.new
+        @memory_checked_callback = options[:memory_checked_callback]
+        @memory_limit_exceeded_callback = options[:memory_limit_exceeded_callback]
         set_timer options[:initial_wait] || @polling_interval
       end
 
       def check_memory_state
-        if @memory_info_provider.bytes > @memory_limit
+        current_memory = @memory_info_provider.bytes
+        if current_memory > @memory_limit
+          @memory_limit_exceeded_callback.call(current_memory, @memory_limit) if @memory_limit_exceeded_callback
           # terminate the world and stop polling
           world.terminate
         else
+          @memory_checked_callback.call(current_memory, @memory_limit) if @memory_checked_callback
           # memory is under the limit - keep waiting
           set_timer
         end
