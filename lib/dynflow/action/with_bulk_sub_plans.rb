8c245abe5723993bc0f698c4231d36b62ae0b9e8
Adam Růžička <adamruzicka@users.noreply.github.com>
2017-12-14 17:36:25 +0100

    Fixes #21716 - Fix cancelling when using bulk actions with polling

--- lib/dynflow/action/with_bulk_sub_plans.rb
+++ lib/dynflow/action/with_bulk_sub_plans.rb
@@ -30,7 +30,8 @@ module Dynflow
 
     def initiate
       output[:planned_count] = 0
-      output[:total_count]   = total_count
+      output[:cancelled_count] = 0
+      output[:total_count] = total_count
       super
     end
 
@@ -58,7 +59,8 @@ module Dynflow
     # The same logic as in Action::WithSubPlans, but calculated using the expected total count
     def run_progress
       if counts_set?
-        (output[:success_count] + output[:failed_count]).to_f / total_count
+        sum = output.values_at(:success_count, :cancelled_count, :failed_count).reduce(:+)
+        sum.to_f / total_count
       else
         0.1
       end
@@ -71,8 +73,8 @@ module Dynflow
     end
 
     def cancel!(force = false)
-      # Count the not-yet-planned tasks as failed
-      output[:failed_count] += total_count - output[:planned_count]
+      # Count the not-yet-planned tasks as cancelled
+      output[:cancelled_count] = total_count - output[:planned_count]
       if uses_concurrency_control
         # Tell the throttle limiter to cancel the tasks its managing
         world.throttle_limiter.cancel!(execution_plan_id)
@@ -92,8 +94,11 @@ module Dynflow
     end
 
     def can_spawn_next_batch?
-      total_count - output.fetch(:planned_count, 0) > 0
+      remaining_count > 0
     end
 
+    def remaining_count
+      total_count - output[:cancelled_count] - output[:planned_count]
+    end
   end
 end
