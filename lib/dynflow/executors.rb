97864b4e4bb71cfc14fc86504d4f961f4ccded01
Adam Ruzicka <aruzicka@redhat.com>
2018-04-25 15:07:11 +0200

    Clear AR connections only if there are no open transactions

--- lib/dynflow/executors.rb
+++ lib/dynflow/executors.rb
@@ -8,9 +8,10 @@ module Dynflow
     # we should wrap it with this method, and we can ensure here to do
     # necessary cleanup, such as cleaning ActiveRecord connections
     def self.run_user_code
+      clear_connections = defined?(::ActiveRecord) && ActiveRecord::Base.connected? && ActiveRecord::Base.connection.open_transactions.zero?
       yield
     ensure
-      ::ActiveRecord::Base.clear_active_connections! if defined? ::ActiveRecord
+      ::ActiveRecord::Base.clear_active_connections! if clear_connections
     end
 
   end
