342cb548b06dea7e3cae3d9c4a5eec7fb4c1f21f
Adam Ruzicka <aruzicka@redhat.com>
2018-06-08 11:14:38 +0200

    Allow middlewares to be executed around hooks

--- lib/dynflow/middleware/stack.rb
+++ lib/dynflow/middleware/stack.rb
@@ -14,7 +14,7 @@ module Dynflow
       @middleware_class = Child! middleware_class, Middleware
       @middleware       = middleware_class.new self
       @action           = Type! action, Dynflow::Action, NilClass
-      @method           = Match! method, :delay, :plan, :run, :finalize, :plan_phase, :finalize_phase, :present
+      @method           = Match! method, :delay, :plan, :run, :finalize, :plan_phase, :finalize_phase, :present, :hook
       @next_stack       = Type! next_stack, Middleware::Stack, Proc
     end
 
