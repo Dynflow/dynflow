67f202f3171e45ed63366ccc31e62869b1eff9dc
Ivan Nečas <inecas@redhat.com>
2019-07-17 14:15:50 +0200

    Prepare the EventWorkItem to be serializable
    Before this patch, the event object within `EventWorkItem` included
    a `resolvable_future`, that could not be serialized (which we need for
    being able to send the work item to sidekiq.
    
    Because of this, we introducing unique `request_id` for the event and
    using it to track the result of the execution back to the resolvable
    future that we are expected to fulfill.
    
    We also needed to postpone creation of event work items to time of other
    work items for the step being processed: the reason is the fact that we
    need to update the step based on the latest result from the executor
    first, otherwise the step status we would have would not be updated and
    we would get error of trying to run event on non-suspended state.

--- lib/dynflow/director/running_steps_manager.rb
+++ lib/dynflow/director/running_steps_manager.rb
@@ -8,11 +8,16 @@ module Dynflow
       def initialize(world)
         @world         = Type! world, World
         @running_steps = {}
-        @events        = WorkQueue.new(Integer, WorkItem)
+        # enqueued work items by step id
+        @work_items    = QueueHash.new(Integer, WorkItem)
+        # enqueued events by step id - we delay creating work items from events until execution time
+        # to handle potential updates of the step object (that is part of the event)
+        @events        = QueueHash.new(Integer, Director::Event)
+        @events_by_request_id = {}
       end
 
       def terminate
-        pending_work = @events.clear.values.flatten(1)
+        pending_work = @work_items.clear.values.flatten(1)
         pending_work.each do |w|
           if EventWorkItem === w
             w.event.result.reject UnprocessableEvent.new("dropping due to termination")
@@ -24,27 +29,38 @@ module Dynflow
         Type! step, ExecutionPlan::Steps::RunStep
         @running_steps[step.id] = step
         # we make sure not to run any event when the step is still being executed
-        @events.push(step.id, work)
+        @work_items.push(step.id, work)
         self
       end
 
       # @returns [TrueClass|FalseClass, Array<WorkItem>]
       def done(step)
         Type! step, ExecutionPlan::Steps::RunStep
-        @events.shift(step.id).tap do |work|
-          work.event.result.fulfill true if EventWorkItem === work
+        # update the step based on the latest finished work
+        @running_steps[step.id] = step
+
+        @work_items.shift(step.id).tap do |work|
+          finish_event_result(work) { |f| f.fulfill true }
         end
 
         if step.state == :suspended
-          return true, [@events.first(step.id)].compact
+          return true, [create_next_event_work_item(step)].compact
         else
-          while (event = @events.shift(step.id))
-            message = "step #{step.execution_plan_id}:#{step.id} dropping event #{event.event}"
-            @world.logger.warn message
-            event.event.result.reject UnprocessableEvent.new(message).
-                tap { |e| e.set_backtrace(caller) }
+          while (work = @work_items.shift(step.id))
+            @world.logger.debug "step #{step.execution_plan_id}:#{step.id} dropping event #{work.request_id}/#{work.event}"
+            finish_event_result(work) do |f|
+              f.reject UnprocessableEvent.new("Message dropped").tap { |e| e.set_backtrace(caller) }
+            end
           end
-          raise 'assert' unless @events.empty?(step.id)
+          while (event = @events.shift(step.id))
+            @world.logger.debug "step #{step.execution_plan_id}:#{step.id} dropping event #{event.request_id}/#{event}"
+            if event.result
+              event.result.reject UnprocessableEvent.new("Message dropped").tap { |e| e.set_backtrace(caller) }
+            end
+          end
+          unless @work_items.empty?(step.id) && @events.empty?(step.id)
+            raise "Unexpected item in @work_items (#{@work_items.inspect}) or @events (#{@events.inspect})"
+          end
           @running_steps.delete(step.id)
           return false, []
         end
@@ -60,19 +76,39 @@ module Dynflow
       # @returns [Array<WorkItem>]
       def event(event)
         Type! event, Event
-        next_work_items = []
 
         step = @running_steps[event.step_id]
         unless step
           event.result.reject UnprocessableEvent.new('step is not suspended, it cannot process events')
-          return next_work_items
+          return []
         end
 
-        can_run_event = @events.empty?(step.id)
-        work          = EventWorkItem.new(event.execution_plan_id, step, event, step.queue)
-        @events.push(step.id, work)
-        next_work_items << work if can_run_event
-        next_work_items
+        can_run_event = @work_items.empty?(step.id)
+        @events_by_request_id[event.request_id] = event
+        @events.push(step.id, event)
+        if can_run_event
+          [create_next_event_work_item(step)]
+        else
+          []
+        end
+      end
+
+      # turns the first event from the queue to the next work item to work on
+      def create_next_event_work_item(step)
+        event = @events.shift(step.id)
+        return unless event
+        work = EventWorkItem.new(event.request_id, event.execution_plan_id, step, event.event, step.queue)
+        @work_items.push(step.id, work)
+        work
+      end
+
+      # @yield [Concurrent.resolvable_future] in case the work item has an result future assigned
+      # and deletes the tracked event
+      def finish_event_result(work_item)
+        return unless EventWorkItem === work_item
+        if event = @events_by_request_id.delete(work_item.request_id)
+          yield event.result if event.result
+        end
       end
     end
   end
