f1f4f8cc198feb82dc22683098dadff225bc512c
Ivan Nečas <inecas@redhat.com>
2019-07-22 21:51:19 +0200

    Add telemetry support for sidekiq

--- lib/dynflow/executors/sidekiq/worker_jobs.rb
+++ lib/dynflow/executors/sidekiq/worker_jobs.rb
@@ -4,16 +4,28 @@ module Dynflow
       module WorkerJobs
         class PerformWork < InternalJobBase
           def perform(work_item)
-            Executors.run_user_code do
-              work_item.world = Dynflow.process_world
-              work_item.execute
+            with_telemetry(work_item) do
+              Executors.run_user_code do
+                work_item.world = Dynflow.process_world
+                work_item.execute
+              end
             end
           rescue Errors::PersistenceError => e
             OrchestratorJobs::HandlePersistenceError.perform_async(e, work_item)
           ensure
-            # TODO AJ: get telemetry back
-            # Dynflow::Telemetry.with_instance { |t| t.increment_counter(:dynflow_worker_events, 1, @telemetry_options) }
             OrchestratorJobs::WorkerDone.perform_async(work_item)
+          end
+
+          private
+
+          def with_telemetry(work_item)
+            Dynflow::Telemetry.with_instance { |t| t.set_gauge(:dynflow_active_workers, +1, telemetry_options(work_item)) }
+            yield
+          ensure
+            Dynflow::Telemetry.with_instance do |t|
+              t.increment_counter(:dynflow_worker_events, 1, telemetry_options(work_item))
+              t.set_gauge(:dynflow_active_workers, -1, telemetry_options(work_item))
+            end
           end
         end
       end
