a6d09932b415b17ddbf7ba7e0aef4ad91543cdaf
Adam Ruzicka <aruzicka@redhat.com>
2019-10-01 13:48:22 +0200

    Make rubocop happy

--- lib/dynflow/executors/sidekiq/core.rb
+++ lib/dynflow/executors/sidekiq/core.rb
@@ -63,20 +63,8 @@ module Dynflow
             super
           else
             # If we're in recovery, we can drop the work as the execution plan will be resumed during validity checks performed when leaving recovery
-            # If we're not in recovery and receive an event from another orchestrator, it means it survived the queue draining. In such case
-            #   we take a look if an execution lock is already being held by an orchestrator (it should be the current one). If no lock is held
-            #   we try to resume the execution plan if possible
-            unless @recovery
-              # We are past recovery now, if we receive an event here, the execution plan will be most likely paused
-              # We can either try to rescue it or turn it over to stopped
-              execution_lock = @world.coordinator.find_locks(class: Coordinator::ExecutionLock.name,
-                                                             id: "execution-plan:#{work.execution_plan_id}").first
-              if execution_lock.nil?
-                plan = @world.persistence.load_execution_plan(work.execution_plan_id)
-                should_resume = !plan.error? || plan.prepare_for_rescue == :running
-                @world.execute(plan.id) if should_resume
-              end
-            end
+            # If we're not in recovery and receive an event from another orchestrator, it means it survived the queue draining.
+            handle_unknown_work_item(work) unless @recovery
           end
         end
 
@@ -104,6 +92,20 @@ module Dynflow
 
         def telemetry_options(queue)
           { queue: queue.to_s, world: @world.id }
+        end
+
+        # We take a look if an execution lock is already being held by an orchestrator (it should be the current one). If no lock is held
+        # we try to resume the execution plan if possible
+        def handle_unknown_work_item(work)
+          # We are past recovery now, if we receive an event here, the execution plan will be most likely paused
+          # We can either try to rescue it or turn it over to stopped
+          execution_lock = @world.coordinator.find_locks(class: Coordinator::ExecutionLock.name,
+                                                         id: "execution-plan:#{work.execution_plan_id}").first
+          if execution_lock.nil?
+            plan = @world.persistence.load_execution_plan(work.execution_plan_id)
+            should_resume = !plan.error? || plan.prepare_for_rescue == :running
+            @world.execute(plan.id) if should_resume
+          end
         end
       end
     end
