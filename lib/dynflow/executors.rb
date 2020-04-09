8db36ec1e5c7a9ceac90c3e16b008c0bb1724eb6
Adam Ruzicka <aruzicka@redhat.com>
2020-04-09 14:20:17 +0200

    Prevent ActiveRecord connections from being leaked
    We hit an issue with Sidekiq where sometimes the ActiveRecord connection pool
    would run out of available connections. I assume this was caused by us
    determining if a connection should be returned too early.
    
    The behavior around cleanup of active record connections is driven by several
    entry (before user code gets executed) and exit (after user code gets executed)
    conditions.
    
    entry-A: The thread currently does not have any connection from the ActiveRecord
    connection pool.
    
    entry-B: The thread has a connection from ActiveRecord connection pool and there
    are no opened transactions associated with the connection.
    
    entry-C: The thread has a connection from ActiveRecord connection pool and there
    is at least one opened transaction already associated with that connection.
    
    After yielding and executing the client code, there are two options:
    
    exit-A: The user code did not need an ActiveRecord connection.
    
    exit-B: The user code used ActiveRecord and either obtained the connection from
    the pool (entry-A) or the thread already had a connection (entry-B or entry-C).
    Either way, the thread has a connection after executing the user code.
    
    Now we're finally getting to the cleanup. It makes sense to perform the cleanup
    if it is safe to do so (entry-A or entry-B) and if the current thread has a
    connection ((entry-A and exit-B) or entry-B).
    
    That leaves us with (entry-A and exit-A) where no cleanup is necessary and
    entry-C where cleanup is not safe to do. We used to perform the cleanup every
    time (including entry-C) and some connection errors came from that because we
    returned the connection in the middle of a transaction.

--- lib/dynflow/executors.rb
+++ lib/dynflow/executors.rb
@@ -4,16 +4,38 @@ module Dynflow
 
     require 'dynflow/executors/parallel'
 
-    # Every time we run a code that can be defined outside of Dynflow,
-    # we should wrap it with this method, and we can ensure here to do
-    # necessary cleanup, such as cleaning ActiveRecord connections
-    def self.run_user_code
-      clear_connections = defined?(::ActiveRecord) && ActiveRecord::Base.connected? && ActiveRecord::Base.connection.open_transactions.zero?
-      yield
-    ensure
-      ::ActiveRecord::Base.clear_active_connections! if clear_connections
-      ::Logging.mdc.clear if defined? ::Logging
-    end
+    class << self
+      # Every time we run a code that can be defined outside of Dynflow,
+      # we should wrap it with this method, and we can ensure here to do
+      # necessary cleanup, such as cleaning ActiveRecord connections
+      def run_user_code
+        # Here we cover a case where the connection was already checked out from
+        # the pool and had opened transactions. In that case, we should leave the
+        # cleanup to the other runtime unit which opened the transaction. If the
+        # connection was checked out or there are no opened transactions, we can
+        # safely perform the cleanup.
+        no_previously_opened_transactions = active_record_open_transactions.zero?
+        yield
+      ensure
+        ::ActiveRecord::Base.clear_active_connections! if no_previously_opened_transactions && active_record_connected?
+        ::Logging.mdc.clear if defined? ::Logging
+      end
 
+      private
+
+      def active_record_open_transactions
+        active_record_active_connection&.open_transactions || 0
+      end
+
+      def active_record_active_connection
+        return unless defined?(::ActiveRecord) && ::ActiveRecord::Base.connected?
+        # #active_connection? returns the connection if already established or nil
+        ::ActiveRecord::Base.connection_pool.active_connection?
+      end
+
+      def active_record_connected?
+        !!active_record_active_connection
+      end
+    end
   end
 end
