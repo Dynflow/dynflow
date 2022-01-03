a2fdd4a991446329e8a5483bcd4e52be4ada579b
Adam Ruzicka <aruzicka@redhat.com>
2022-01-03 11:34:02 +0100

    Stub timezone in tests

--- test/extensions_test.rb
+++ test/extensions_test.rb
@@ -5,37 +5,40 @@ require 'active_support/time'
 module Dynflow
   module ExtensionsTest
     describe 'msgpack extensions' do
-      before do
-        Thread.current[:time_zone] = ActiveSupport::TimeZone['Europe/Prague']
-      end
-      after { Thread.current[:time_zone] = nil }
-
       it 'allows {de,}serializing Time' do
-        time = Time.now
-        transformed = MessagePack.unpack(time.to_msgpack)
-        assert_equal transformed, time
-        assert_equal transformed.class, time.class
+        ::Time.stub(:zone, ActiveSupport::TimeZone['Europe/Prague']) do
+          time = Time.now
+          transformed = MessagePack.unpack(time.to_msgpack)
+          assert_equal transformed, time
+          assert_equal transformed.class, time.class
+        end
       end
 
       it 'allows {de,}serializing ActiveSupport::TimeWithZone' do
-        time = Time.zone.now
-        transformed = MessagePack.unpack(time.to_msgpack)
-        assert_equal transformed, time
-        assert_equal transformed.class, time.class
+        ::Time.stub(:zone, ActiveSupport::TimeZone['Europe/Prague']) do
+          time = Time.zone.now
+          transformed = MessagePack.unpack(time.to_msgpack)
+          assert_equal transformed, time
+          assert_equal transformed.class, time.class
+        end
       end
 
       it 'allows {de,}serializing DateTime' do
-        time = DateTime.now
-        transformed = MessagePack.unpack(time.to_msgpack)
-        assert_equal transformed, time
-        assert_equal transformed.class, time.class
+        ::Time.stub(:zone, ActiveSupport::TimeZone['Europe/Prague']) do
+          time = DateTime.now
+          transformed = MessagePack.unpack(time.to_msgpack)
+          assert_equal transformed, time
+          assert_equal transformed.class, time.class
+        end
       end
 
       it 'allows {de,}serializing Date' do
-        date = DateTime.current
-        transformed = MessagePack.unpack(date.to_msgpack)
-        assert_equal transformed, date
-        assert_equal transformed.class, date.class
+        ::Time.stub(:zone, ActiveSupport::TimeZone['Europe/Prague']) do
+          date = DateTime.current
+          transformed = MessagePack.unpack(date.to_msgpack)
+          assert_equal transformed, date
+          assert_equal transformed.class, date.class
+        end
       end
     end
   end
