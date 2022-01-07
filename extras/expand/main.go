8dd87a2fd2643bfbf6fddb6abaeec2a690d7fb28
Adam Ruzicka <aruzicka@redhat.com>
2022-01-07 13:23:40 +0100

    Simplify the expand tool

--- extras/expand/main.go
+++ extras/expand/main.go
@@ -4,47 +4,17 @@ import (
 	"encoding/csv"
 	"encoding/hex"
 	"encoding/json"
-	"github.com/vmihailenco/msgpack"
+	"github.com/vmihailenco/msgpack/v5"
 	"io"
 	"os"
 )
 
-// dynflow_steps
-// 0                   1  2         3    4     5          6        7         8              9             10              11    12    13           14       15
-// execution_plan_uuid,id,action_id,data,state,started_at,ended_at,real_time,execution_time,progress_done,progress_weight,class,error,action_class,children,queue
-//
-// encoded columns are:
-// 3 - data
-// 12 - error
-// 14 - children
-
-// dynflow_actions
-// 0                   1  2    3                        4                5     6     7      8            9           10
-// execution_plan_uuid,id,data,caller_execution_plan_id,caller_action_id,class,input,output,plan_step_id,run_step_id,finalize_step_id
-//
-// encoded columns are:
-// 2 - data
-// 6 - input
-// 7 - output
-
-// dynflow_execution_plans
-// Without msgpack
-// 0    1    2     3      4          5        6         7              8     9     10       11            12                13                14
-// uuid,data,state,result,started_at,ended_at,real_time,execution_time,label,class,run_flow,finalize_flow,execution_history,root_plan_step_id,step_ids
-
-// With msgpack
-// 0    1    2     3      4          5        6         7              8     9     10                11       12            13                14
-// uuid,data,state,result,started_at,ended_at,real_time,execution_time,label,class,root_plan_step_id,run_flow,finalize_flow,execution_history,step_ids
-//
-// 1 - data
-// 11 - run_flow
-// 12 - finalize_flow
-// 13 - execution_history
-// 14 - step_ids
-
 func main() {
 	reader := csv.NewReader(os.Stdin)
+	defer os.Stdin.Close()
+
 	writer := csv.NewWriter(os.Stdout)
+	defer os.Stdout.Close()
 	defer writer.Flush()
 
 	for {
@@ -58,31 +28,12 @@ func main() {
 }
 
 func processRow(record []string) []string {
-	// Execution plan exports have 15 fields, other exports have different counts
-	if len(record) == 15 {
-		record = expandExecutionPlan(record)
-	}
-
 	for i, r := range record {
-		record[i] = reencodeField(r)
+		if isHexEncoded(r) {
+			record[i] = reencodeField(r)
+		}
 	}
 
-	return record
-}
-
-func expandExecutionPlan(record []string) []string {
-	var flow_columns [2]int
-
-	// The step_ids field should be a safe indicator
-	if isHexEncoded(record[14]) {
-		flow_columns = [...]int{11, 12}
-	} else {
-		flow_columns = [...]int{10, 11}
-	}
-
-	for _, i := range flow_columns {
-		record[i] = expandFlow(record[i])
-	}
 	return record
 }
 
@@ -91,38 +42,18 @@ func isHexEncoded(field string) bool {
 }
 
 func reencodeField(field string) string {
-	decoded, err := decode(field)
+	decoded_bytes, err := hex.DecodeString(field[2:])
 	if err != nil {
 		return field
 	}
 
-	return encode(decoded)
-}
-
-func decode(field string) (interface{}, error) {
 	var intermediate interface{}
-	bytes := []byte(field)
-
-	if isHexEncoded(field) {
-		decoded_bytes, err := hex.DecodeString(field[2:])
-		if err != nil {
-			return "", err
-		}
-
-		err = msgpack.Unmarshal(decoded_bytes, &intermediate)
-		if err != nil {
-			return "", err
-		}
-
-		return intermediate, nil
-	}
-
-	err := json.Unmarshal(bytes, &intermediate)
+	err = msgpack.Unmarshal(decoded_bytes, &intermediate)
 	if err != nil {
-		return "", err
+		return field
 	}
 
-	return intermediate, nil
+	return encode(intermediate)
 }
 
 func encode(data interface{}) string {
@@ -132,49 +63,4 @@ func encode(data interface{}) string {
 	}
 
 	return string(result)
-}
-
-func expandFlow(field string) string {
-	intermediate, err := decode(field)
-	if err != nil {
-		return field
-	}
-
-	var result map[string]interface{}
-	switch intermediate.(type) {
-	// old style hash
-	case map[string]interface{}:
-		result = intermediate.(map[string]interface{})
-	// newer compact S-expression like representation
-	case []interface{}, float64:
-		result = expandCompactFlow(intermediate)
-	}
-
-	return encode(result)
-}
-
-func expandCompactFlow(flow interface{}) map[string]interface{} {
-	result := make(map[string]interface{})
-	switch flow.(type) {
-	case []interface{}:
-		switch flow.([]interface{})[0] {
-		case "S":
-			result["class"] = "Dynflow::Flows::Sequence"
-		case "C":
-			result["class"] = "Dynflow::Flows::Concurrence"
-		default:
-			panic("Unknown flow type")
-		}
-		var subflows []interface{}
-		for subflow := range flow.([]interface{})[1:] {
-			subflows = append(subflows, expandCompactFlow(subflow))
-		}
-		result["flows"] = subflows
-	case float64, int:
-		result["class"] = "Dynflow::Flows::Atom"
-		result["step_id"] = flow
-	default:
-		panic("Unknown flow type")
-	}
-	return result
 }
