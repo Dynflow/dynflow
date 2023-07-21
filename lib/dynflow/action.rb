3d4d8e6fd3b3952f1c470a6dda761d93e42ca8a4
Adam Ruzicka <aruzicka@redhat.com>
2023-07-21 13:00:50 +0000

    Introduce v2 for sub-plans
    The previous implementation of sub plans API comprised of several
    modules which could be layered on top of each other. This made the code
    somewhat hard to follow, not all of the modules worked in all scenarios
    as some relied on an executor being a single process.
    
    The v2 flattens all the modules into a single one and drastically
    reduces the amount of possible combinations. Sub plans are spawned in
    batches and then the parent polls until the sub plans finish.
    
    It also includes support for limitting the number of sub-plans that may
    be running at a given time, without relying on the throttle limitter and
    thus should be safe in multi-process environments. Under the hood it
    dynamically alters the batch size to honor the concurrency limit.

--- lib/dynflow/action.rb
+++ lib/dynflow/action.rb
@@ -26,6 +26,7 @@ module Dynflow
     require 'dynflow/action/with_sub_plans'
     require 'dynflow/action/with_bulk_sub_plans'
     require 'dynflow/action/with_polling_sub_plans'
+    require 'dynflow/action/v2'
 
     def self.all_children
       children.values.inject(children.values) do |children, child|
