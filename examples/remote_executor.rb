be0238fbcc7e4cf0a6319985fe57950123bdc3a2
Adam Ruzicka <aruzicka@redhat.com>
2024-01-29 10:54:00 +0100

    Fix Layout/HeredocIndentation cop

--- examples/remote_executor.rb
+++ examples/remote_executor.rb
@@ -143,17 +143,17 @@ command = ARGV.first || 'server'
 if $0 == __FILE__
   case command
   when 'observer'
-    puts <<MSG
-The observer starting…. You can see what's going on there
-MSG
+    puts <<~MSG
+      The observer starting…. You can see what's going on there
+    MSG
     RemoteExecutorExample.run_observer
   when 'server'
-    puts <<MSG
-The server is starting…. You can send the work to it by running:
-
-   #{$0} client
-
-MSG
+    puts <<~MSG
+      The server is starting…. You can send the work to it by running:
+      
+         #{$0} client
+      
+    MSG
     RemoteExecutorExample.run_server
   when 'client'
     RemoteExecutorExample.run_client
