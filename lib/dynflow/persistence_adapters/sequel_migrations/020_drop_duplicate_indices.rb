b688025cd1e41f6408e06c28f45c9c1cc0bb1681
Jakub Duchek <jakduch@seznam.cz>
2026-09-11 21:15:45 +0200

    fixed index name derivation

--- lib/dynflow/persistence_adapters/sequel_migrations/020_drop_duplicate_indices.rb
+++ lib/dynflow/persistence_adapters/sequel_migrations/020_drop_duplicate_indices.rb
@@ -2,21 +2,16 @@
 
 Sequel.migration do
   up do
-    if indexes(:dynflow_actions).key?(:dynflow_actions_execution_plan_uuid_id_index)
-      alter_table(:dynflow_actions) do
-        drop_index [:execution_plan_uuid, :id]
-      end
-    end
+    [
+      [:dynflow_actions, [:execution_plan_uuid, :id]],
+      [:dynflow_execution_plans, :uuid],
+      [:dynflow_steps, [:execution_plan_uuid, :id]],
+    ].each do |table, columns|
+      index_name = default_index_name(table, Array(columns)).to_sym
+      next unless indexes(table).key?(index_name)
 
-    if indexes(:dynflow_execution_plans).key?(:dynflow_execution_plans_uuid_index)
-      alter_table(:dynflow_execution_plans) do
-        drop_index :uuid
-      end
-    end
-
-    if indexes(:dynflow_steps).key?(:dynflow_steps_execution_plan_uuid_id_index)
-      alter_table(:dynflow_steps) do
-        drop_index [:execution_plan_uuid, :id]
+      alter_table(table) do
+        drop_index columns
       end
     end
   end
